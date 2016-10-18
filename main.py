ca3de7fce838b9207cb0a50bd917ab07d2a49f21
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2016-10-18 14:18:02 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+class AudioToMIDI():
+	def __init__(self, audiofile):
+		self.audiofile = audiofile
+	def run_fft():
+		# read audiofile
+		# run fft
+		# set self.notes or something like that to a 2d array of notes happening at every timestep
+	def convert_midi():
+		# save self.notes as a midi
