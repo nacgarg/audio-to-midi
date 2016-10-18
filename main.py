f3e3792eb906674e84dbca09f8e95c125e944f6f
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2016-10-18 14:21:35 -0700

    added stuff

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from scipy.io.wavfile import read
+from scipy.fftpack import fft
+import numpy as np
+
 class AudioToMIDI():
-	def __init__(self, audiofile):
-		self.audiofile = audiofile
+	def __init__(self, audiofile, sample_rate=44100, fft_size=600):
+		a = read(audiofile)
+		self.audio = np.array(a[1],dtype=float)
+		self.timestep = 1.0 / sample_rate
 	def run_fft():
-		# read audiofile
 		# run fft
 		# set self.notes or something like that to a 2d array of notes happening at every timestep
 	def convert_midi():
